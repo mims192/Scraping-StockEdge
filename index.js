a75c56a01c19affc050e0c9c77bc9280ae1d8ca4
Mimansa Sahay <mimansa192@gmail.com>
2025-04-30 12:59:16 +0530

    update monhly delivery file

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import { scrapeDividendInfo } from "./corp_action.js";
 import scrape from "./daily.js";
 import feed from "./feed.js";
 import scrapeFPI from "./FPI_activity.js";
-import scrapeMonthly from "./Monthly.js";
 import scrapeMonthly2 from "./Monthly2.js";
+import scrapeMonthly from "./Monthlydelivery.js";
 import news from "./news.js";
 
 scrape();
 news();
-scrapeMonthly();
+scrapeMonthly(); 
 scrapeDividendInfo();
 scrapeMonthly2();
 scrapeFPI();
