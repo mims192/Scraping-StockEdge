8de1bf1de18dc67c483b28d72a39c82db6ade872
Mimansa Sahay <142939055+mims192@users.noreply.github.com>
2025-04-30 13:52:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ import scrapeMonthly2 from "./Monthly2.js";
 import scrapeMonthly from "./Monthlydelivery.js";
 import news from "./news.js";
 
+import { getStocksFromCSV } from './stocklist.js';
+const stocks = await getStocksFromCSV();
+
 async function runScrapersSequentially() {
   try {
-    await scrape();           
-    await news();                
-    await scrapeMonthly();      
-    await scrapeDividendInfo();  
-    await scrapeMonthly2();     
-    await scrapeFPI();          
-    await feed();                
+    await scrape(stocks);           
+    await news(stocks);                
+    await scrapeMonthly(stocks);      
+    await scrapeDividendInfo(stocks);  
+    await scrapeMonthly2(stocks);     
+    await scrapeFPI(stocks);          
+    await feed(stocks);                
     console.log(" All scrapers completed.");
   } catch (err) {
     console.error(" Error running scrapers:", err);
