ec8df7ee8d191fc890d2881852fd38d065a04b79
Mimansa Sahay <mimansa192@gmail.com>
2025-04-30 12:54:11 +0530

    updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { scrapeDividendInfo } from "./corp_action.js";
 import scrape from "./daily.js";
 import feed from "./feed.js";
 import scrapeFPI from "./FPI_activity.js";
-import scrapeMonthly from "./monthly.js";
+import scrapeMonthly from "./Monthly.js";
 import scrapeMonthly2 from "./Monthly2.js";
 import news from "./news.js";
 
